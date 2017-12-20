1e72c2d21c538971b41e559a837d1b15cc3c4e93
Rohan Varma <rvarm1@gmail.com>
2017-12-20 15:55:28 -0800

    add illustration of loss functions

--- cs231n/loss.py
+++ cs231n/loss.py
@@ -69,7 +69,56 @@ def L(X, y, W):
   return np.sum(margins) - n_examples
 
 
+def make_softmax_graph(x_probs):
+  # precondition: x is an np.array of increasing probs between 0 and 1
+  # the label will be 0, 1, ... len(x_probs) - 1
+  x_probs = np.arange(start = 0.01, stop = 1.0, step = 0.02)
+  labels = np.array([0 for _ in x_probs])
+  softmax_one = lambda x, labels: -(labels.dot(np.log(x_probs)))
+  softmax_two = lambda x, labels: -(labels.dot(np.log(x_probs)) + (1-labels).dot(np.log(1-x_probs)))
+  first_way, second_way = [], []
+  for idx, _ in enumerate(x_probs):
+    # assign the correct label
+    if idx == 0:
+      labels[idx] = 1
+    else:
+      labels[idx-1], labels[idx] = 0, 1 #deassign the old label
+    first_way.append(softmax_one(x_probs, labels))
+    second_way.append(softmax_two(x_probs, labels))
+  # verify the losses are at least monotic dec
+  # and softmax 2 loss is higher than softmax 1 loss? 
+  for idx, val in enumerate(zip(first_way, second_way)):
+    assert val[0] < val[1]
+    assert idx == 0 or first_way[idx] < first_way[idx-1]
+    assert idx == 0 or second_way[idx] < second_way[idx-1]
+  import matplotlib.pyplot as plt
+  line_first, = plt.plot(x_probs, first_way)
+  line_second, = plt.plot(x_probs, second_way)
+  print('abs diff in loss for first way {}, for second way {}'.format(first_way[0]-first_way[-1], second_way[0]-second_way[-1]))
+  line_first.set_label('y_j * log(h(x_i))')
+  line_second.set_label('y_j * log(h(x_i)) + (1-y_j) * log(1 - h(x_i))')
+  plt.legend()
+  plt.xlabel('probability of correct class')
+  plt.ylabel('loss')
+  plt.show()
+  return first_way, second_way
+
+
+
+
 if __name__ == '__main__':
+  # have 3 classes
+  x_probs = np.array([0.2, 0.4, 0.6, 0.8, 0.9, 0.95, 0.99])
+  labels = np.array([0 for _ in x_probs])
+  labels[0] = 1
+  softmax_one = lambda x, labels: -(labels.dot(np.log(x_probs)))
+  assert softmax_one(x_probs, labels) == -np.log(0.2), "output {} but got {}".format(softmax_one(x_probs, labels), np.log(0.2))
+  print(softmax_one(x_probs, labels))
+  softmax_two = lambda x, labels: -(labels.dot(np.log(x_probs)) + (1-labels).dot(np.log(1-x_probs)))
+  print(-(np.log(0.2) + np.sum(np.log(1-np.array(x_probs.tolist()[1:])))))
+  print(softmax_two(x_probs, labels))
+  make_softmax_graph(x_probs)
+  exit()
   y = [2, 3, 1, 0, 9]
   num_classes = 10
   print(one_hot(y, 10))
@@ -80,5 +129,6 @@ if __name__ == '__main__':
   W = np.random.rand(10, 10)
   y = [0, 8, 6, 3, 9]
   print(L(X, y, W))
+
 
 
